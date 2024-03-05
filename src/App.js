4327d72886f3b04f02dd16df2006397508b0e3c3
yoonsangjo <131744090+yoonsangjo@users.noreply.github.com>
2024-03-05 15:39:20 +0900

    ## INTRODUCTION - Introduction to Hooks, Requirements, Workflow

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+class AppUgly extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello</h1>
+      </>
+    );
+  }
+}
+
+export default App;
